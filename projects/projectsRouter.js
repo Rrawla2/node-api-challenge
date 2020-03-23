03f92175eb4e4c705e529c1b99cb6db558611386
Kristi Gribble <kgribble4083@gmail.com>
2020-03-22 21:21:29 -0500

    Removed package-lock, added get, post, delete

---
+++ projects/projectsRouter.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const Projects = require("../data/helpers/projectModel");
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+    console.log("projectsRouter")
+    next();
+})
+
+router.get('/', (req, res) => {
+    Projects.get()
+        .then(project => {
+            res.status(200).json(project)
+            })
+        .catch(err => {
+            res.status(500).json({ error: "This project could not be retrieved" })
+        })
+}); 
+
+router.get('/:id', validateProjectID, (req, res) => {
+    res.status(200).json(req.project)
+})
+
+router.post('/', (req, res) => {
+    const project = req.body
+    Projects.insert(project)
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Error adding Project" })
+        })
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    Projects.remove(id)
+        .then(project => {
+            if(project) {
+                res.status(200).json({ message: "The Project has been removed" })
+            } else {
+                res.status(404).json({ message: "The Project could not be found" })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: "There was an error removing the Project" })
+        })
+})
+
+function validateProjectID(req, res, next) {
+    const {id} = req.params
+    Projects.get(id)
+        .then(project => {
+            console.log("Project", project)
+            if(project) {
+                req.project = project
+                next();
+            } else {
+                res.status(400).json({ message: "Project ID not found" })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Project ID could not be validated" })
+        })
+}
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const projectsRouter = require("./projects/projectsRouter");
-const actionsRouter = require("./actions/actionsRouter");
+
 
 const server = express();
 
 server.use(express.json());
 server.use(logger);
 server.use('/api/projects', projectsRouter);
-server.use('api/actions', actionsRouter);
+
 
 server.get('/', (req, res) => {
     res.send(`<h2>View the projects</h2>`);
